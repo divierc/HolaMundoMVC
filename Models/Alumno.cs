b8c8a3508c227472deff853fa283e6f331baf44b
divierc <53800137+divierc@users.noreply.github.com>
2021-09-02 19:17:06 -0400

    Se agragan los modelos del repositorio JuanKRuiz

---
+++ Models/Alumno.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace CoreEscuela.Entidades
+{
+    public class Alumno: ObjetoEscuelaBase
+    {
+        public List<Evaluación> Evaluaciones { get; set; } = new List<Evaluación>();
+    }
+}
