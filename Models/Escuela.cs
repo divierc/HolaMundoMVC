3d322ba4a2007b1479d68b38e547735902f70b27
Divier Castaneda <divierc@msn.com>
2021-09-01 23:15:36 -0400

    Se creo el modelo escuela

---
+++ Models/Escuela.cs
@@ -0,0 +1,14 @@
+namespace HolaMundoMVC.Models
+{
+    public class Escuela
+    {
+        public string EscuelaId { get; set; }
+
+        public string Nombre { get; set; }
+        
+        public int AñoFundación { get; set; }
+        
+              
+        
+    }
+}
