66b62d872c701c5c4c19ba63ae2e50b3c4ff2dbd
divierc <53800137+divierc@users.noreply.github.com>
2021-09-04 20:48:01 -0400

    Crecion de vista parcial Asignatura

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using HolaMundoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -8,14 +9,22 @@ namespace HolaMundoMVC.Controllers
 {
     public class AsignaturaController : Controller
     {
-         public IActionResult Index()
+        public IActionResult Index(){
+            return View(new Asignatura() { Nombre="Matematicas" });
+
+        }
+         public IActionResult MultiAsignatura()
         {
-            // Se agrega el modelo escuela
-            var asignatura=new Asignatura();
-            asignatura.Nombre = "Matematicas";
+            // Se agrega la lista de asignaturas
+            var ListaAsignaturas = new List<Asignatura>(){
+                    new Asignatura() { Nombre="Matematicas" },
+                    new Asignatura() { Nombre="Educacion Fisica" },
+                    new Asignatura() { Nombre="Castellano" },
+                    new Asignatura() { Nombre="Ciencias Naturales" }
+                };
             
             ViewBag.FechaAsignatura = DateTime.Now;
-            return View(asignatura);
+            return View("MultiAsignatura",ListaAsignaturas);
         }
 
     }
