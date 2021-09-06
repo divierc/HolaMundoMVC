7732b38f273abeb8e2b8241784340fc5985e1da8
divierc <53800137+divierc@users.noreply.github.com>
2021-09-06 19:46:03 -0400

    Se creo dbContext en Startup.cs, Creo EcuelaContext.cs y modifico EscuelaController.cs con el nuevo context.

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -2,26 +2,24 @@ using System;
 using System.Runtime.InteropServices;
 using HolaMundoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 
 namespace HolaMundoMVC.Controllers
 {
     public class EscuelaController : Controller
     {
+        private EscuelaContext _context;
+
          public IActionResult Index()
         {
-            // Se agrega el modelo escuela
-            var escuela=new Escuela();
-            escuela.Nombre= "Platzi Academy";
-            escuela.TipoEscuela = TiposEscuela.Primaria;
-            escuela.Dirección = "Calle 1 #12-32";
-            escuela.Pais= "Colombia";
-            escuela.Ciudad = "Bogota";
-            escuela.AñoDeCreación = 2005;
-            
-            
             ViewBag.TextoDePrueba = "Cualquier texto";
+            var escuela=_context.Escuelas.FirstOrDefault();
             return View(escuela);
+        }
+        public EscuelaController(EscuelaContext context)
+        {
+            _context=context;
         }
 
     }
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HolaMundoMVC.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,8 @@ namespace HolaMundoMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            // Se conffiguran los servidios de inmemory
+            services.AddDbContext<EscuelaContext>(options => options.UseInMemoryDatabase(databaseName: "testDB"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
